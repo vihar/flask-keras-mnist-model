010fe225bf4af2f1811f78ac27f764cead450989
vihar <vihar.kurama@gmail.com>
2018-07-02 12:49:47 +0530

    Canvas Configured

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import keras
+import tensorflow as tf
+import pandas as pd
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "Hello World"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
