bedfc0758cb81c9cad9adbea71070ddc0df74cec
vihar <vihar.kurama@gmail.com>
2018-07-02 15:01:43 +0530

    Model Trained and Saved

--- app.py
+++ app.py
@@ -1,16 +1,51 @@
-from flask import Flask, render_template
-import keras
-import tensorflow as tf
-import pandas as pd
+from flask import Flask, render_template,request
+from scipy.misc import imsave, imread, imresize
+import numpy as np
+import keras.models
+import re
+import sys 
+import os
+sys.path.append(os.path.abspath("./model"))
+from load import * 
 
+
+global graph, model
+
+model, graph = init()
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index_view():
     return render_template('index.html')
 
+def convertImage(imgData1):
+	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
+	with open('output.png','wb') as output:
+		output.write(imgstr.decode('base64'))
+
+@app.route('/predict/',methods=['GET','POST'])
+def predict():
+	imgData = request.get_data()
+	convertImage(imgData)
+
+	x = imread('output.png',mode='L')
+
+	x = np.invert(x)
+
+	x = imresize(x,(28,28))
+
+
+	x = x.reshape(1,28,28,1)
+
+	with graph.as_default():
+		out = model.predict(x)
+		print(out)
+		print(np.argmax(out,axis=1))
+
+		response = np.array_str(np.argmax(out,axis=1))
+		return response	
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
---
+++ train.py
@@ -0,0 +1,80 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+img_rows, img_cols = 28, 28
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+#build our model
+model = Sequential()
+#convolutional layer with rectified linear unit activation
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+#again
+model.add(Conv2D(64, (3, 3), activation='relu'))
+#choose the best features via pooling
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#randomly turn neurons on and off to improve convergence
+model.add(Dropout(0.25))
+#flatten since too many dimensions, we only want a classification output
+model.add(Flatten())
+#fully connected to get all relevant data
+model.add(Dense(128, activation='relu'))
+#one more dropout for convergence' sake :) 
+model.add(Dropout(0.5))
+#output a softmax to squash the matrix into output probabilities
+model.add(Dense(num_classes, activation='softmax'))
+#Adaptive learning rate (adaDelta) is a popular form of gradient descent rivaled only by adam and adagrad
+#categorical ce since we have multiple classes (10) 
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+#train that ish!
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+ #how well did it do? 
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+#Save the model
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
